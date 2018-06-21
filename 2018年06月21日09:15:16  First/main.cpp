07633d5bd54b8c8ecc7a92b05d5e2be3d2fd1c37
20171105130 <S20171105130@admindeimac-36.local>
2018-06-21 09:17:23 +0800

    Initial Commit

---
+++ 2018年06月21日09:15:16  First/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2018年06月21日09:15:16  First
+//
+//  Created by 杨扬 on 18/6/21.
+//  Copyright © 2018年 杨扬. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
